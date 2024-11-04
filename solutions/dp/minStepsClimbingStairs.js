a25628954ba73b1ec23f9823b34d001b3a62ccc7
João Luis Moraes <jonmoraesNL@gmail.com>
2024-11-04 19:56:50 -0300

    remove dp

--- solutions/dp/minStepsClimbingStairs.js
+++
@@ -1,8 +0,0 @@
-// neetcode
-var minCostClimbingStairs = function (cost) {
-  for (let i = cost.length - 3; i >= 0; i--) {
-    cost[i] = Math.min(cost[i] + cost[i + 1], cost[i] + cost[i + 2]);
-  }
-
-  return Math.min(cost[0], cost[1]);
-};
